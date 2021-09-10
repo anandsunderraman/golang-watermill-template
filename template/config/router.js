3f9bb9cf7f8403ba9a91acdf208f65b25e38e5be
asunderraman <anand.sunderraman@gmail.com>
2021-09-10 12:53:12 -0400

    refactoring router.js

--- template/config/router.js
+++ template/config/router.js
@@ -1,10 +1,18 @@
 const { File } = require('@asyncapi/generator-react-sdk');
-import { Router } from '../../components/ListChannels';
+import { Router } from '../../components/Router';
+import { GetSubscriberFlags } from '../../components/common';
 
 export default async function({ asyncapi, params }) {
+
+  let subscriberFlags = GetSubscriberFlags(asyncapi);
+
+  if (!subscriberFlags.hasAMQPSub) {
+      return
+  }
+
   return (
     <File name="router.go">
-        <Router moduleName={params.moduleName} channels={asyncapi.channels()} />    
+        <Router moduleName={params.moduleName} channels={asyncapi.channels()} subscriberFlags={subscriberFlags} />    
     </File>
   );
 }
