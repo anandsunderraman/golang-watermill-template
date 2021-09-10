58ea126f33d3679030df0e498776b463d7332ae2
asunderraman <anand.sunderraman@gmail.com>
2021-09-10 10:01:34 -0400

    adding subscriber

---
+++ template/config/subscriber.js
@@ -0,0 +1,30 @@
+const { File } = require('@asyncapi/generator-react-sdk');
+import { Subscriber } from '../../components/ListChannels';
+
+export default async function({ asyncapi, params }) {
+
+    const channelEntries = Object.keys(asyncapi.channels()).length ? Object.entries(asyncapi.channels()) : [];
+    //if there are no channels do nothing
+    if (channelEntries.length === 0) {
+        return
+    }
+
+    //if there are no subscribers then do nothing
+    let hasAMQPSubscriber = channelEntries.filter(([channelName, channel]) => {
+        return channel.hasPublish() && channel.bindings().amqp
+    }).length > 0;
+
+    if (!hasAMQPSubscriber) {
+        return
+    }
+
+    let subscriberFlags = {
+        hasAMQPSubscriber: hasAMQPSubscriber
+    }
+
+    return (
+      <File name="subscribers.go">
+         <Subscriber subscriberFlags={subscriberFlags} />    
+      </File>
+    );
+  }
