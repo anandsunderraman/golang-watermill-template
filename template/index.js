c788f077644996a8dcfd3aaeba60660f39a26434
asunderraman <anand.sunderraman@gmail.com>
2021-09-10 11:08:09 -0400

    cleaning up index.js

--- template/index.js
+++ template/index.js
@@ -1,9 +1,5 @@
-import { File, render } from '@asyncapi/generator-react-sdk';
+import { File } from '@asyncapi/generator-react-sdk';
 
-// Import custom components from file 
-import { HTML, Head, Body } from '../components/common';
-import { ListChannels } from '../components/ListChannels';
-import { DiagramContent } from '../components/DiagramContent';
 
 //render an AMQP subscriber
 function AMQPSubscriber() {
@@ -93,61 +89,4 @@ func main() {
 `}
     </File>
   );
-}
-
-/*
- * Below you can see how reusable chunks could be called in another way.
- * If you need process the React component to string you should use `render` function from `@asyncapi/generator-react-sdk` package.
- * This function transforms given component (and its children) and returns pure string.
- * 
- * Notice also how to retrieve passed properties to custom component, by the destruction of the first argument.
- * Accessing document data is made easier thanks to what AsyncAPI JavaScript Parser is doing to the AsyncAPI document.
- */
-function BodyContent({ asyncapi }) {
-  const apiName = asyncapi.info().title();
-  const channels = asyncapi.channels();
-
-  return `
-<div class="container mx-auto px-4">        
-  <p> 
-    <h1>${apiName}</h1>
-    ${render(<ListChannels channels={channels} operationType='subscribe' />)}
-    ${render(<ListChannels channels={channels} operationType='publish' />)}
-    ${render(<DiagramContent asyncapi={asyncapi} />)}
-    ${render(<Extension asyncapi={asyncapi} />)}
-    ${render(<ExternalDocs asyncapi={asyncapi} />)}
-  </p> 
-</div>  
-`;
-}
-
-/*
- * This is an example how you can access values from AsyncAPI file from its extensions
- */
-function Extension({ asyncapi }) {
-  if (!asyncapi.info().hasExt('x-twitter')) return null;
-  return `Share your feedback with us on <a href="http://twitter.com/${asyncapi.info().ext('x-twitter')}">Twitter</a>.`;
-}
-
-function ExternalDocs({ asyncapi }) {
-  if (!asyncapi.hasExternalDocs()) return null;
-  const url = asyncapi.externalDocs().url();
-  return `Don't forget to visit our website <a href="${url}">${url}</a>.`;
-}
-
-/*
- * You can access "maxTextSide" parameter value without any conditions in case user didn't provide such a parameter. 
- * It is possible thanks to the functionality that makes it possible for template developer to specify default values for parameters.
- * Check out package.json file and look for `generator.parameters.maxTextSize` and its description and default value.
- */
-function Scripts({ params }) {
-  return `
-<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
-<script>
-  mermaid.initialize({
-    startOnLoad: true,
-    maxTextSize: ${params.maxTextSize},
-  });
-</script>
-`;
 }
